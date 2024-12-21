3f70cb265f6950f9597cdb9df52706c3e800cc93
Poornakgps <103123710+Poornakgps@users.noreply.github.com>
2024-12-21 09:12:47 +0530

    Update app.js

--- app.js
+++ app.js
@@ -36,15 +36,6 @@ db.sequelize.authenticate()
         console.error('Unable to connect to the database:', err);
     });
 
-
-
-// Serve static files in production
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
-    app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
-    });
-}
 // app.get('/', (req, res) => {
 //     res.send('Hello World!')
 // }
